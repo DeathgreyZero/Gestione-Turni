31974c95f5c575a991b504ac4a14363248026d48
kalel13 <anto.cal91@gmail.com>
2015-07-01 19:04:42 +0200

    Test POST

--- gestione/views.py
+++ gestione/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
+    print 'Index'
+    if request.method == 'POST':
+        user = request.POST['user']
+        password = request.POST['password']
+        login(user,password)
+
     return render(request,'gestione/index.html')
+
+
+
+def login(user,password):
+    print "Username "+user+"Pass: "+password
