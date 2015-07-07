0b46347cd1e8f4df55f45667ee7e98dd93d6e73b
kalel13 <anto.cal91@gmail.com>
2015-07-07 14:49:34 +0200

    rest

--- gestione/generate_html.py
+++ gestione/generate_html.py
@@ -64,26 +64,31 @@ def calcola_turno(reparto):
     nome = ''
     global ultimo_turno
     #ogni giorno diminuisce di 1 i contatori del riposo e del giorno
-
+    print str(numero_turni)+'<<<<turni'
     if numero_turni%6 == 0:
+        print ">>>>>>>>>>>>>>>>SCALO I RIPOSI<<<<<<<<<<<<<<<<<<<"
         query = Persona.objects.filter(count_giorno = 0)
         for count in xrange(1,len(persone)+1):
             r = Persona.objects.get(pk = persone[count-1].matricola)
             if r.count_giorno > 0 and not query:
                 r.count_giorno -=1
                 r.save()
-            elif r.riposo > 0:
+            elif r.riposo <= 3 and r.riposo > 0:
                 r.riposo -=1
+                r.save()
+            elif r.riposo == 0:
+                r.ultimo_turno = 1
                 r.save()
 
     for count in xrange(1,len(persone)+1):
         p = Persona.objects.get(pk = persone[count-1].matricola)
-        print 'calcolo > '+str(count)+' prendo: '+p.nome
-        if p.riposo < 1 and p.count_giorno <= 1:
+        print 'calcolo -> '+str(count)+' prendo: '+p.nome
+        if p.riposo == 0:
             if reparto == 1:
-                if p.count_giorno == 0:
+                if p.count_giorno <= 1 and p.ultimo_turno%2 == 0:
                     nome = p.nome
                     p.count_giorno = 1
+                    p.riposo = 1
                     p.ultimo_turno = 1
                     p.save()
                     print 'idoneo, salvo '+p.nome
@@ -91,7 +96,15 @@ def calcola_turno(reparto):
                 print 'non idoneo'
                 print '1'
             elif reparto == 2:
-                nome = '2'
+                if p.count_giorno <= 1:
+                    nome = p.nome
+                    p.riposo = 3
+                    p.ultimo_turno = 2
+                    p.save()
+                    print 'idoneo, salvo '+p.nome
+                    return nome
+                print 'non idoneo'
+                print '2'
             elif reparto == 3:
                 nome = '3'
             elif reparto == 4:
@@ -100,9 +113,6 @@ def calcola_turno(reparto):
                 nome = '5'
             elif reparto == 6:
                 nome = '6'
-
-        elif p.count_giorno > 0:
-            continue
     return nome
 
 
@@ -114,10 +124,13 @@ counter = 1
 for days in tupla4:
     mat.put(cordinate(counter-1, 0),str(counter)+' '+str(days))
     table = table + '<td class = "tg-031e">'+mat[counter-1][0]+'</td>'
+    print '>>>>>>>>>>>>>>GIORNO '+str(counter)+'<<<<<<<<<<<<<<<<<<<<<'
     for reparto in range(1,7):
+        print '>>>>>>>>>>>>> REPARTO: '+str(reparto)+'<<<<<<<<<<<<<<<<'
         turno = calcola_turno(reparto)
         table = table+'<td class="tg-vn4c">' + turno + '</td>'
         mat.put(cordinate(counter-1,reparto),turno)
+        numero_turni +=1
     table = table + "</tr>"
     counter += 1
 
